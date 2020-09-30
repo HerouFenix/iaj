65259f668c4884ec89095c4478f3c29151d409e8
HerouFenix <diogo04@ua.pt>
2020-09-30 22:14:21 +0100

    Lab1 Done

---
+++ Lab1/IAJ Movimento 1/Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicArrive.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicArrive : DynamicVelocityMatch
+    {
+        public float StopRadius { get; set; }
+        public float SlowRadius { get; set; }
+
+        public DynamicArrive()
+        {
+            base.MovingTarget = new KinematicData();
+        }
+
+        public override string Name
+        {
+            get { return "Arrive"; }
+        }
+
+        public override MovementOutput GetMovement()
+        {
+            Vector3 direction = this.Target.position - this.Character.position;
+            float distance = direction.magnitude;
+
+            // By default we'll stop
+            float desiredSpeed = 0;
+
+            // If we're outside the slow radius go at max speed
+            if (distance > this.SlowRadius)
+            {
+                desiredSpeed = this.MaxAcceleration;
+                Debug.Log("Going at full speed");
+            } // If we're inside the slow radius but outside the stop radius, slow down
+            else if (distance >= this.StopRadius)
+            {
+                desiredSpeed = this.MaxAcceleration * (distance / this.SlowRadius);
+                Debug.Log("Slowing down");
+            }
+
+            base.MovingTarget.velocity = direction.normalized * desiredSpeed;
+
+            return base.GetMovement();
+        }
+    }
+}
---
+++ Lab1/IAJ Movimento 1/Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicWander.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.IAJ.Unity.Util;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicWander : DynamicSeek
+    {
+        public DynamicWander()
+        {
+            this.Target = new KinematicData();
+        }
+        public override string Name
+        {
+            get { return "Wander"; }
+        }
+        public float TurnAngle { get; set; }
+        public float WanderOffset { get; set; }
+        public float WanderRadius { get; set; }
+        public float WanderRate { get; set; }
+        protected float WanderOrientation { get; set; }
+        public Vector3 CircleCenter { get; private set; }
+        public GameObject DebugTarget { get; set; }
+
+        public override MovementOutput GetMovement()
+        {
+            // I wander (get it) if there's something missing here...
+            this.TurnAngle += RandomHelper.RandomBinomial() * this.WanderRate;
+
+            this.WanderOrientation = this.TurnAngle + this.Character.orientation;
+
+            this.CircleCenter = this.Character.position + this.WanderOffset * MathHelper.ConvertOrientationToVector(this.Character.orientation);
+
+            base.Target.position = this.CircleCenter + this.WanderRadius * MathHelper.ConvertOrientationToVector(this.WanderOrientation);
+
+            // Debug.Log("Target Pos: " + base.Target.position);
+            ////////////////////////
+
+            if (this.DebugTarget != null)
+            {
+                this.DebugTarget.transform.position = this.Target.position;
+            }
+
+            return base.GetMovement();
+        }
+    }
+}
