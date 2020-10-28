82fb8063b631478e9fb27f264d46201152e4c536
HerouFenix <diogo04@ua.pt>
2020-10-28 18:11:06 +0000

    finished lab3, started lab4

---
+++ Lab4/IAJ Pathfinding Grid 2/Assets/Scripts/IAJ.Unity/Pathfinding/DataStructures/NodeRecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures
+{
+    public enum NodeStatus
+    {
+        Unvisited,
+        Open,
+        Closed
+    }
+
+    public class NodeRecord  : IComparable<NodeRecord>
+    {
+        //Coordinates
+        public int x;
+        public int y;
+
+        public NodeRecord parent;
+        public float gCost;
+        public float hCost;
+        public float fCost;
+        public NodeStatus status;
+        public bool isWalkable;
+
+        public override string ToString()
+        {
+            return x + "," + y;
+        }
+
+        public int CompareTo(NodeRecord other)
+        {
+            return this.fCost.CompareTo(other.fCost);
+        }
+
+        public NodeRecord(int x, int y)
+        {
+            
+            this.x = x;
+            this.y = y;
+            gCost = int.MaxValue;
+            hCost = 0;
+            fCost = gCost + hCost;
+            parent = null;
+            isWalkable = true;
+            status = NodeStatus.Unvisited;
+        }
+        public void CalculateFCost()
+        {
+            fCost = gCost + hCost;
+        }
+    }
+}
